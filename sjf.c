6e3660bfa53fb29444ee6e516f87df9aa8f9e361
Cody K <Z_10535969@cs3060.ad.uvu.edu>
2015-10-29 16:11:31 -0600

    push more sjf.c code

--- sjf.c
+++ sjf.c
@@ -1,6 +1,18 @@
+//Primary Coder: Cody Kellogg	
+//Group project
+//CS 3060-601 Fall 2015
+//Assignment #5
+/* Promise of Originality
+I promise that this source code file has, in it's entirety, been
+written by myself and by no other person or persons. If at any time an
+exact copy of this source code is found to be used by another person in
+this term, I understand that both myself and the student that submitted
+the copy will receive a zero on this assignment.
+*/
+ 
 #include"assn5.h"
 #include<stdio.h>
-const int INT_MAX = 65000;
+const int INT_MAX = 650000;
 const int array_size = 100;
 const int check = -1;
 
