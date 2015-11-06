063454524ea794d6b4d423e15a212fa6695e9139
Zachary H <Z_10655997@cs3060.ad.uvu.edu>
2015-11-05 23:40:11 -0700

    Final Attempt

--- srtf.c
+++ srtf.c
@@ -13,13 +13,11 @@ the copy will receive a zero on this assignment.
 #include<stdio.h>
 
 
+const int MAX_SIZE = 100;
 void srtf(int count, int *sub, int *run)
 {
-	const int MAX_SIZE = 100;
-	// Put start times and runs in two arrays.  Keep these arrays synced
-	//int sub [MAX_SIZE] = sub;
-	//int run [MAX_SIZE] = run;
-	//int wait_queue[MAX_SIZE];
+	int remaining_process_times[100];
+	int end_times[100];
 	int current = 0;
 	int x = 0;
 	int time_left_current = 0;
@@ -28,50 +26,62 @@ void srtf(int count, int *sub, int *run)
 	int avg_wait_time = 0;
 	int avg_response_time =0;
 	int avg_turnaround_time=0;
-	// Calculate average wait time
-	while (x < count)
+
+	while (current < 4)
 	{
-		// Find how much time has elapsed from the start of the current process to the introduction of the newest process
+		x = current;
+		// Find how much time elapses between the start of the current and next processes
 		elapsed = sub[current + 1] - sub[current];
+		// If there is enough time to run the process to completion
 		if (elapsed > run[current])
 		{
+			// Add zero to wait time because there was no waiting for this process
 			avg_wait_time += 0;
-		}
-		else
-		{
-			time_left_current = run[current] - elapsed;
-			x = current + 1;
-			while (time_left_current > 0)
+			// There is no remaining time to process so fill this entry with zero
+			remaining_process_times[current] = 0;
+			// Store end times if the process for later calculations
+			end_times[current] = sub[current] + run[current];
+			// Track turnaround time by subtracting the ending time by the start time
+			avg_turnaround_time += (end_times[current] - sub[current]);
+			// See if there is extra time to finish previously preempted processes
+			while (x > 0)
 			{
-				elapsed = sub[x + 1] - sub[x];
-				if (elapsed > run[x])
+				// If there is no remaining process time for this entry, move on
+				if (remaining_process_times[x] == 0)
 				{
-					elapsed = elapsed - run[x];
-					time_left_current = time_left_current - elapsed;
-					/*while (time_left_others > 0 && elapsed > run[x])
-					{
-						elapsed--;
-						time_left_others--;
-					}
-					if (elapsed > run[x])
-					{
-						//elapsed = elapsed - run[x];
-						time_left_current = time_left_current - elapsed;
-					}*/
+					x = x-1;
 				}
 				else
 				{
-					time_left_others = time_left_others + run[x] - elapsed;
+					// If there is time to finish a remaining process 
+					if (elapsed - remaining_process_times[x] > 0)
+					{
+						avg_wait_time += end_times[x] - sub[x-1];
+						// Indicaate process is finished
+						remaining_process_times[x] = 0;
+						avg_turnaround_time += end_times[x] - sub[x];
+						x = x - 1;
+					}
+					// If there isn't time to finish a remaining process, do as much as possible
+					else
+					{
+						remaining_process_times[x] = remaining_process_times[x] - elapsed;
+						x = 0;
+					}
 				}
-				avg_wait_time += run[x++];
 			}
-
+		}
+		// If the process doesn't have a shorter remaining time
+		else
+		{
+		
+			remaining_process_times[x] = run[x] - elapsed;
+			avg_wait_time += run[x + 1];
 		}
 		current++;
-
-		
 	}
 	printf("Shortest Remaining Time First\n");
+	// Spit out some random numbers that I can't seem to fix no matter what I try
 	printf("Avg. Resp.:%.2f, Avg. T.A:%.2f, Avg. Wait:%.2f\n",
 		(float)avg_response_time/ count, (float)avg_turnaround_time / count, (float)avg_wait_time / count);
 }
